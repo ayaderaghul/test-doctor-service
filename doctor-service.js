a494fef07d497744ba2bff5d9cc2f6424d833ceb
Aya de Raghũl <nguyenlinhchi09@gmail.com>
2025-01-21 16:27:51 +0700

    Update doctor-service.js

--- doctor-service.js
+++ doctor-service.js
@@ -2,9 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { Sequelize, DataTypes } = require("sequelize");
 const pg = require("pg");
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+
+
+app.use(cors());
+
 
 // Database setup
 const sequelize = new Sequelize("postgresql://test_nq2p_user:jgICFMuHQqYbjcblCvCkjVSJx7VGZw0L@dpg-cu7f0clumphs73d5ieu0-a:5432/test_nq2p", {
